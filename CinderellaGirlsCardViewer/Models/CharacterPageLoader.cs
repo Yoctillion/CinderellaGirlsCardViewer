44f52e13b6b5c55b8dd3aa8c7f652b0a447a5aad
Utsuho <lwpqq000@gmail.com>
2016-09-13 21:34:27 +0900

    Add random parameter for query

--- CinderellaGirlsCardViewer/Models/CharacterPageLoader.cs
+++ CinderellaGirlsCardViewer/Models/CharacterPageLoader.cs
@@ -17,14 +17,13 @@ namespace CinderellaGirlsCardViewer.Models
 
         private string _nextPageUrl;
 
-
         internal CharacterPageLoader(HttpClient client, string keyword)
         {
             const string queryUrl =
-                "http://sp.pf.mbga.jp/12008305/?guid=ON&url=http%3A%2F%2F125.6.169.35%2Fidolmaster%2Fgallery%2Findex%2F0%2F0%2F%3Fkeyword%3D";
+                "http://sp.pf.mbga.jp/12008305/?guid=ON&url=http%3A%2F%2F125.6.169.35%2Fidolmaster%2Fgallery%2Findex%2F0%2F0%2F%3Fkeyword%3D{0}%26l_frm%3DGallery_1%26rnd%3D{1}";
 
             this._client = client;
-            this._nextPageUrl = queryUrl + WebUtility.UrlEncode(keyword);
+            this._nextPageUrl = string.Format(queryUrl, WebUtility.UrlEncode(keyword), new Random().Next().ToString());
         }
 
         public async Task<bool> LoadNextPage()
