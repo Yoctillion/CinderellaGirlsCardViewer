4145355b82b0be22c370140d7c9bc3751cfd6580
Utsuho <lwpqq000@gmail.com>
2016-09-26 23:49:07 +0900

    Revert "Update SimpleCache"
    This reverts commit a78a5539c726fff171f9a3475fe52aefe3e8b2e3.

--- CinderellaGirlsCardViewer/SimpleCache.cs
+++ CinderellaGirlsCardViewer/SimpleCache.cs
@@ -1,55 +1,30 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
+using System.Threading.Tasks;
 
 namespace CinderellaGirlsCardViewer
 {
-    internal class SimpleCache<TKey, TValue> : IDisposable
+    internal class SimpleCache<TKey, TValue>
     {
-        private readonly Dictionary<TKey, Lazy<TValue>> _inner = new Dictionary<TKey, Lazy<TValue>>();
-        private readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+        private readonly ConcurrentDictionary<TKey, Lazy<TValue>> _inner = new ConcurrentDictionary<TKey, Lazy<TValue>>();
 
         public TValue Get(TKey key, Func<TKey, TValue> factory)
         {
-            return this._lock.LockUpgradeableRead(() =>
-            {
-                Lazy<TValue> lazy;
-                if (!this._inner.TryGetValue(key, out lazy))
-                {
-                    lazy = this.UpdateInternal(key, factory);
-                }
-                return lazy;
-            }).Value;
+            return this._inner.GetOrAdd(key, new Lazy<TValue>(() => factory(key))).Value;
         }
 
         public TValue Update(TKey key, Func<TKey, TValue> factory)
         {
-            return this.UpdateInternal(key, factory).Value;
-        }
-
-        private Lazy<TValue> UpdateInternal(TKey key, Func<TKey, TValue> factory)
-        {
-            return this._lock.LockWrite(() =>
-            {
-                var lazy = new Lazy<TValue>(() => factory(key));
-                this._inner[key] = lazy;
-                return lazy;
-            });
+            return this._inner.AddOrUpdate(key, new Lazy<TValue>(() => factory(key)), (o, n) => n).Value;
         }
 
         public TValue Remove(TKey key)
         {
-            Lazy<TValue> lazy = null;
-            this._lock.LockUpgradeableRead(() =>
-            {
-                if (this._inner.TryGetValue(key, out lazy))
-                {
-                    this._lock.LockWrite(() => this._inner.Remove(key));
-                }
-            });
-
+            Lazy<TValue> lazy;
+            this._inner.TryRemove(key, out lazy);
             return (lazy != null && lazy.IsValueCreated)
                 ? lazy.Value
                 : default(TValue);
@@ -58,11 +33,6 @@ namespace CinderellaGirlsCardViewer
         public void Clear()
         {
             this._inner.Clear();
-        }
-
-        public void Dispose()
-        {
-            this._lock.Dispose();
         }
     }
 }
